a0357e6fbe8643bfacfac9116f60e021e0fc578e
Christian Roach <compcritter@gmail.com>
2017-08-25 13:24:26 -0400

    a

---
+++ src/main/java/com/revature/dao/ApartmentComplexDAO.java
@@ -0,0 +1,9 @@
+package com.revature.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.revature.beans.ApartmentComplex;
+
+public interface ApartmentComplexDAO extends CrudRepository<ApartmentComplex, Integer>{
+
+}
