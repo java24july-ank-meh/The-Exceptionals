aa5ba43f908510b654ebcdd6c4037c9efc717396
Huriel Hernandez <huriel.hernandez01@utrgv.edu>
2017-08-27 16:50:29 -0400

    Feature ( Add loading spinner )

--- src/main/resources/static/Complexes/complexesController.js
+++ src/main/resources/static/Complexes/complexesController.js
@@ -1,59 +1,12 @@
 angular.module('rhmsApp').controller('complexesController', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog', '$http', function($scope, $mdBottomSheet, $mdSidenav, $mdDialog, $http) {
 
-    $scope.complexes = [
-        {
-            id: 1,
-            name : "Apartment Complex Name",
-            phone: "9563133128",
-            email: "mail@mail.com",
-            address: "474 Elden St, Herndon, VA 20170",
-            website: "www.site.com"
-        },
-        {
-            id: 2,
-            name : "Apartment Complex Name",
-            phone: "9563133128",
-            email: "mail@mail.com",
-            address: "474 Elden St, Herndon, VA 20170",
-            website: "www.site.com"
-        },
-        {
-            id: 3,
-            name : "Apartment Complex Name",
-            phone: "9563133128",
-            email: "mail@mail.com",
-            address: "474 Elden St, Herndon, VA 20170",
-            website: "www.site.com"
-        },
-        {
-            id: 4,
-            name : "Apartment Complex Name",
-            phone: "9563133128",
-            email: "mail@mail.com",
-            address: "474 Elden St, Herndon, VA 20170",
-            website: "www.site.com"
-        },
-        {
-            id: 6,
-            name : "Apartment Complex Name",
-            phone: "9563133128",
-            email: "mail@mail.com",
-            address: "474 Elden St, Herndon, VA 20170",
-            website: "www.site.com"
-        },
-        {
-            id: 5,
-            name : "Apartment Complex Name",
-            phone: "9563133128",
-            email: "mail@mail.com",
-            address: "474 Elden St, Herndon, VA 20170",
-            website: "www.site.com"
-        }
-    ];
-
+	$scope.error = false;
+	
      $http.get("/api/ApartmentComplexes").then(function(response) {
          $scope.complexes = response.data;
-         console.log($scope.complexes);
+         
+         if($scope.complexes === '')
+        	 $scope.error = true;
      });
 
 }]);
