f9edb9a127b3e6e703b806c59410e51507e0ff80
Christian Roach <compcritter@gmail.com>
2017-08-29 19:18:11 -0400

    Table updated to include residents without apartments

--- src/main/resources/static/Residents/residentsController.js
+++ src/main/resources/static/Residents/residentsController.js
@@ -1,48 +1,38 @@
 angular.module('rhmsApp').controller('residentsController', ['$scope', '$mdBottomSheet','$http', '$mdDialog','$stateParams','$state' ,'$mdToast', '$filter', 'NgTableParams', function($scope, $mdBottomSheet,$http, $mdDialog, $stateParams, $state, $mdToast, $filter, NgTableParams) {
-		//function ($scope, $filter, NgTableParams, $http) {
 	
-	
-	/*$scope.residentsTable = new NgTableParams({}, { getData: function(response) {
-		return $http.get("/api/Residents").then(function(response) {
-			console.log(response.data)
-	        return response.data;        
-	    });
-	}});*/
-	
-	/*$http.get("/api/Residents").then(function(response) {
-		$scope.residentsTable = new NgTableParams({}, { dataset: response.data});    
-		//gonna have to make a new call base on ids to retrieve parents.
-    });*/
-	//$scope.test = [{id: "", title: ""}, {id: 'Moroni', title: 'Moroni'}, {id: 'Enos', title: 'Enos'}, {id: 'Nephi', title: 'Nephi'}];
+	$http.get("/api/Residents").then(function(response) {
+		$scope.residents = response.data;
+    });
+
 	$http.get("/api/ApartmentComplexes").then(function(response) {
-		$scope.complexes = [];
+		
+		
 		$scope.residentData = [];
 		response.data.forEach(function(complex) {
-			$scope.complexes.push({id: complex.name, title: complex.name})
 			if(complex.apartments){
 				complex.apartments.forEach(function(apartment) {
-					
 					if(apartment.residents){ 
 						apartment.residents.forEach(function(resident) {
-							resident.apartmentNumber = apartment.apartmentNumber;
-							resident.complexName = complex.name;
-							$scope.residentData.push(resident);
-							
+							$scope.residents.forEach(function(trueResident) {
+								if (trueResident.apartmentNumber === undefined){
+									trueResident.apartmentNumber = 0;
+									trueResident.complexName = "none";
+								}
+									
+								if (trueResident.residentId === resident.residentId) {
+									trueResident.apartmentNumber = apartment.apartmentNumber;
+									trueResident.complexName = complex.name;
+								}
+							});
 						});
-					} else {
-						$scope.residentData.push({"firstName":"No resident","apartment":apartment.apartmentNumber.toString(),"complexName":complex.name});
 					}
 				});
-			} else {
-				$scope.residentData.push({"apartment":"No Apartment","complexName":complex.name});
 			}
 		});
 		console.log($scope.complexes)
-		$scope.residentsTable = new NgTableParams({}, { dataset: $scope.residentData});	
+		$scope.residentsTable = new NgTableParams({}, { dataset: $scope.residents});	
 		console.log($scope.residentData);
-		//$scope.complexes = response.data;
 	});
-	//$scope.complexes = $scope.names = [{id: "", title: ""}, {id: 'Moroni', title: 'loloooollo'}, {id: 'Enos', title: 'Enos'}, {id: 'Nephi', title: 'Nephi'}];
 	
 	$scope.showCreateResidentForm = function(ev) {
 		  
