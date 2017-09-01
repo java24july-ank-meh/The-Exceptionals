e22c591f70803d582c6c521aad2aacdfacac2661
RobertoAlvarez94 <roberto_utb@hotmail.com>
2017-08-31 21:12:10 -0400

    some changes to configuration to deploy

--- src/main/java/com/revature/application/HousingManagementSystemApplication.java
+++ src/main/java/com/revature/application/HousingManagementSystemApplication.java
@@ -2,10 +2,17 @@ package com.revature.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HousingManagementSystemApplication {
+public class HousingManagementSystemApplication extends SpringBootServletInitializer {
 
+	 	@Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(HousingManagementSystemApplication.class);
+	    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HousingManagementSystemApplication.class, args);
 	}
